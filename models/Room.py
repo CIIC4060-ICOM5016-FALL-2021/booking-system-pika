0d1ed0400aa714ff77d16b3ea5b60352b1f57cce
Juanma <juan.gonzalez63@upr.edu>
2021-11-14 22:30:51 -0400

    small print

--- models/Room.py
+++ models/Room.py
@@ -59,6 +59,7 @@ class RoomDAO:
         query = "insert into room (r_building, r_dept, r_type)  values (%s, %s, %s) returning r_id; "
         cursor.execute(query, (r_building, r_dept, r_type,))
         r_id = cursor.fetchone()[0]
+        print(r_id)
         self.conn.commit()
         return r_id
 
