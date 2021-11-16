2ad4e65a3b5f0da1df20d55b8110e63022b07283
Detective Pikachu <frenzyexists@gmail.com>
2021-11-16 01:53:05 -0400

    yes

--- app.py
+++ app.py
@@ -63,6 +63,14 @@ def get_most_used_room():
 def get_room_by_person_id(p_id):
     pass
 
+@app.route('/pika-booking/rooms/unavailable-time-rooms', methods=['GET'])
+def get_unavailable_time_rooms():
+    pass
+
+@app.route('/pika-booking/rooms/verify-time-frame', methods=['GET'])
+def get_verify_time_frame():
+    pass
+
 
 # ======================= #
 # ===-| P E R S O N |-=== #
@@ -73,6 +81,33 @@ def handle_users(username):
         return Person().create_new_person(request.json)
     else:
         return Person().get_all_persons()
+
+
+# ================================= #
+# ===-| A V A I A B I L I T Y |-=== #
+# ================================= #
+@app.route('/pika-booking/persons/unavailable-time-users', methods=['GET'])
+def handle_available_time_of_users():
+    if request.method == 'GET':
+        return Person().get_all_available_time_persons()
+    else:
+        return jsonify("Method Not Allowed"), 405
+
+
+@app.route('/pika-booking/persons/unavailable-time-users/<int:p_id>', methods=['GET'])
+def handle_available_time_of_users_by_id(p_id):
+    if request.method == 'GET':
+        return Person().get_all_available_time_persons(p_id)
+    else:
+        return jsonify("Method Not Allowed"), 405
+
+
+@app.route('/pika-booking/persons/<int:p_id>/unavailable-time-slot/', methods=['POST'])
+def handle_person_available(p_id):
+    if request.method == 'POST':
+        return Person().create_unavailable_person_time_frame(p_id, request.json)
+    else:
+        return jsonify("Method Not Allowed"), 405
 
 
 # ========================= #
