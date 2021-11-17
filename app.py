325c3898fc029d3683ee64f5d160301f47ba51d4
shulk72 <franklin.sifre@upr.edu>
2021-11-16 20:10:08 -0400

    make create and get all un available person

--- app.py
+++ app.py
@@ -112,10 +112,10 @@ def handle_persons_role_by_id(p_id):
         return jsonify("Method Not Allowed"), 405
 
 
-@app.route('/pika-booking/persons/schedule/<int:p_id>', methods=['GET'])
-def handle_persons_schedule_by_id(p_id):
+@app.route('/pika-booking/persons/schedule/', methods=['GET'])
+def handle_persons_schedule_by_id():
     if request.method == 'GET':
-        return Person().get_all_day_schedule_of_person(p_id, request.json)
+        return Person().get_all_day_schedule_of_person(request.json)
     else:
         return jsonify("Method Not Allowed"), 405
 
