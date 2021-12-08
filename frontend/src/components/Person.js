908519940a1e6b07dbb36c215daed8d47b4c3f85
shulk72 <franklin.sifre@upr.edu>
2021-12-08 01:20:59 -0400

    made person.js

--- frontend/src/components/Person.js
+++ frontend/src/components/Person.js
@@ -15,15 +15,38 @@ axios.get('https://booking-system-pika.herokuapp.com/pika-booking/persons').then
     this.setState({person: Per});
 })
     }
+
+    gender(parameter){
+        switch(parameter) {
+            case 1:
+                return "Male"
+            case 2:
+                return "Female"
+
+        }
+    }
+
+    role(parameter){
+        switch(parameter) {
+            case 1:
+                return "Student"
+            case 2:
+                return "Professor"
+            case 3:
+                return "Staff"
+            default:
+                return "Visitor"
+        }
+    }
     render() {
         return <>
 
      {this.state.person.map(Per=>
                 <Card>
             <label>{Per.p_fname} _ {Per.p_lname},
-                <p>Role: {Per.p_role}</p>
+                <p>Role: {this.role(Per.p_role)}</p>
                 <p> Email: {Per.p_email}</p>
-               Gender: {Per.p_gender}
+               Gender: { this.gender(Per.p_gender)}
              </label>
                     <Button basic color='green'>
                         Invite
