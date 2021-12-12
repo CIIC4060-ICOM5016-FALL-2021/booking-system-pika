4fc5311b9931bcf8b3442f499c9441ac04a19d51
shulk72 <franklin.sifre@upr.edu>
2021-12-12 00:39:24 -0400

    fixed to get most shared name

--- frontend/src/UserStatistics.js
+++ frontend/src/UserStatistics.js
@@ -4,6 +4,7 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment';
 import {Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis} from "recharts";
 import axios, {Axios} from "axios";
+import {data}  from "/"
 import {Button,
     Divider,
     Grid,
@@ -19,11 +20,10 @@ import {Button,
 import {Link, Route} from "react-router-dom";
 import bookMeeting from "./BookMeeting";
 import Navbar from "./components/Navbar/Navbar";
-import UserView from "./UserView";
 function UserStatistics (){
     let [data,setdata] =  useState("");
     let [d,setd] =  useState("");
-
+    let[name,setname] =  useState("");
 
         axios.post('https://booking-system-pika.herokuapp.com/pika-booking/persons/person/most-booked-room', {"p_id": '5'}).then(res => {
             setdata(res.data);
@@ -33,13 +33,16 @@ function UserStatistics (){
             setd(res.data);
         })
 
-
+       axios.post('https://booking-system-pika.herokuapp.com/pika-booking/persons/id',{"p_id": d.p_id}).then(res =>{
+           setname(res.data)
+       })
 
         return <>
+            <Navbar/>
                 <h1>  Most Used Room By You:
-                    {data.r_building}
+             <h1>  Building: {data.r_building} ,  Dept: {data.r_dept}  , Type: {data.r_type}  </h1>
                 </h1>
-            <h1>User most booked with You:    {d.p_id}</h1>
+            <h1>User most booked with You:    {name.p_fname} {name.p_lname}</h1>
                         <Link to = "/Dashboard" > <button>
                 Go to Dashboard
             </button>
