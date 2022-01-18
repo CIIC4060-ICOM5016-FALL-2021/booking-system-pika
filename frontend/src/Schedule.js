978967f742009ef1abf731810da8be2314bcaa97
shulk72 <franklin.sifre@upr.edu>
2022-01-18 00:58:31 -0400

    lots of fixes

--- frontend/src/Schedule.js
+++ frontend/src/Schedule.js
@@ -33,11 +33,11 @@ function Schedule(){
                     const st =` ${res.data.st_dt[i]}-0400 (Atlantic Standard Time)`
                     const et = ` ${res.data.et_dt[i]}-0400 (Atlantic Standard Time)`
                     let tile =""
-                    if ( res.data.booking_name[i]===""){
+                  //  if ( res.data.booking_name[i]===""){
                         tile = "Unavailable"
-                    }else{
-                        tile = res.data.booking_name[i]
-                    }
+                  //  }else{
+                  //      tile = res.data.booking_name[i]
+                  //  }
 
                 const w = {title: tile, start:  new Date(st), end :  new Date(et), room: 4}
                 t.push(w)
