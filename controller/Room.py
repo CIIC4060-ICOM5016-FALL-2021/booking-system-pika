7820119d952f45ba45b71ddc5ba9f8bb0f424393
Detective Pikachu <frenzyexists@gmail.com>
2022-01-17 22:12:34 -0400

    Rollback

--- controller/Room.py
+++ controller/Room.py
@@ -1,121 +1,140 @@
 from flask import jsonify
-
-from models.AvailableRoom import AvailableRoomDAO
 from models.Room import RoomDAO
 
 
-def get_all_rooms(limit_thingy=25):
-    method = RoomDAO()
-    count = method.count_rooms()
-    if count != 0:
-        data = method.get_all_rooms(limit_thingy)
-        result: list = [{'count': count}]
-        for row in data:
-            result.append({
-                'r_id': row[0],
-                'room_name': row[1],
-                'room_type': method.rooms[row[2]],
-                'url': 'https://booking-system-pika.herokuapp.com/pika-booking/rooms/' + str(row[0])
-            })
-        return jsonify(result), 200
-    else:
-        return jsonify("There are no Rooms around"), 404
+class Room:
 
+    # Generate the Rows
+    def build_room(self, row: tuple):
+        result = {
+            "r_building": row[0],
+            "r_dept": row[1],
+            "r_type": row[2]
+        }
+        return result
 
-def create_new_room(json: dict):
-    r_building = json['r_building']
-    r_dept = json['r_dept']
-    r_name = json['r_name']
-    r_type = json['r_type']
-    method = RoomDAO()
-    r_id = method.create_new_room(r_name, r_building, r_dept, r_type)
-    return jsonify({"r_id": r_id})
+    def build_room1(self, row: tuple):
+        result = {
+            "r_id": row[0],
+            "r_building": row[1],
+            "r_dept": row[2],
+            "r_type": row[3]
+        }
+        return result
+    # Overloading
+    def build_room_attr_dict(self, r_id, r_building, r_dept, r_type):
+        return self.build_room((r_id, r_building, r_dept, r_type))
 
+    def build_timeslot(self, row: tuple):
+        print(row, "ROW")
+        result = {
+            "r_id": row[0],
+            "st_dt": row[1],
+            "et_dt": row[2]
+        }
+        return result
 
-def update_room(json: dict):
-    r_id = json['r_id']
-    r_building = json['r_building']
-    r_dept = json['r_dept']
-    r_name = json['r_name']
-    r_type = json['r_type']
-    method = RoomDAO()
-    existing_room = method.check_if_room_exists(r_id)
-    if existing_room:
-        result = method.update_room(r_id, r_name, r_building, r_dept, r_type)
-        return jsonify(result), 200
-    else:
-        return jsonify("Room Not Found"), 404
+    def build_timeslot_attr_dict(self, r_id, st_dt, et_dt):
+        return self.build_timeslot((r_id, st_dt, et_dt))
 
+    # Read
+    #
+    # Gets All Rooms
 
-def delete_room(r_id: int):
-    method = RoomDAO()
-    result = method.delete_room(r_id)
-    if result:
-        method2 = AvailableRoomDAO()
-        method2.delete_unavailable_room(r_id)
-        return jsonify("Room Deleted Successfully"), 200
-    else:
-        return jsonify("Room Not Found"), 404
+    def get_all_rooms(self):
+        dao = RoomDAO()
+        all_rooms = dao.get_all_rooms()
+        if not all_rooms:
+            return jsonify("There's no rooms! It feels, lonely.."), 404
+        else:
+            result = []
+            for row in all_rooms:
+                result.append(self.build_room1(row))
+            return jsonify(result)
 
+    # Returns a query of most booked rooms
+    # example: {r_id: serial, r_count: int}, in descending order
+    def get_most_booked_rooms(self):
+        method = RoomDAO()
+        booked_rooms = method.get_most_booked_rooms()
+        if not booked_rooms:
+            return jsonify("There's either no Bookings or no Rooms created"), 404
+        else:
+            result = []
+            for row in booked_rooms:
+                result.append({
+                    "r_id": row[0],
+                    "timed_booked": row[1]
+                })
+            return jsonify(result), 200
 
-def get_room(room):
-    method = RoomDAO()
-    if type(room) == int:
-        if method.check_if_room_exists(room):
-            data = method.get_room_by_id(room)
-            return jsonify({
-                'r_name': data[0],
-                'r_building': data[1],
-                'r_dept': data[2],
-                'r_type': method.rooms[data[3]]
-            })
+    # Create
+    #
+    # Creates a new Room]
+    # It first checks if exists using id
+    #
+    def create_new_room(self, json):
+        # r_id = json['r_id']
+        r_building = json['r_building']
+        r_dept = json['r_dept']
+        r_type = json['r_type']
+        dao = RoomDAO()
+        # existing_room = dao.get_room(r_id)
+
+        # Room with such name dos not exist
+        # if not existing_room:
+        room_id = dao.create_new_room(r_building, r_dept, r_type)
+        result = self.build_room_attr_dict(room_id, r_building, r_dept, r_type)
+        return jsonify(result), 201
+        # else:
+        #     return jsonify("A room with that ID already exists"), 409
+
+    # Delete
+    def delete_room(self, r_id):
+        dao = RoomDAO()
+        result = dao.delete_room(r_id)
+        if result:
+            return jsonify("Room Deleted Successfully"), 200
         else:
             return jsonify("Room Not Found"), 404
-    elif type(room) == str:
-        data = method.get_room_by_name(room)
-        if not data:
-            return jsonify("Room does not exist"), 404
+
+    # Update
+    def update_room(self, json):
+        r_id = json['r_id'] # 1
+        r_building = json['r_building'] # "ADEM"
+        r_dept = json['r_dept'] # "adem"
+        r_type = json['r_type'] # 3
+        dao = RoomDAO()
+        existing_room = dao.get_room(r_id)
+        if not existing_room:
+            return jsonify("Room Not Found"), 404
         else:
-            return jsonify({
-                'r_id': data[0],
-                'r_building': data[1],
-                'r_dept': data[2],
-                'r_type': method.rooms[data[3]]
-            })
+            dao.update_room(r_id, r_building, r_dept, r_type)
+            result = self.build_room_attr_dict(r_id, r_building, r_dept, r_type)
+            return jsonify(result), 200
 
+    # Put a number and get some room thingy
+    def get_room_by_id(self, r_id):
+        dao = RoomDAO()
+        rooms_by_id = dao.get_room(r_id, )
+        if not rooms_by_id:
+            return jsonify("There's no rooms!"), 404
+        else:
+            print(rooms_by_id)
+            result = self.build_room(rooms_by_id)
+            return jsonify(result), 200
 
-def get_most_booked_rooms():
-    method = RoomDAO()
-    booked_rooms = method.get_most_booked_rooms()
-    if not booked_rooms:
-        return jsonify("There's either no Bookings or no Rooms created"), 404
-    else:
-        result: list = []
-        for row in booked_rooms:
-            result.append({
-                "r_id": row[0],
-                "room_name": row[1],
-                "timed_booked": row[2],
-                "url": 'https://booking-system-pika.herokuapp.com/pika-booking/rooms/' + str(row[0])
-            })
-        return jsonify(result), 200
-
-
-# Retrieves all available rooms
-def get_available_rooms(json):
-    st_dt = json['st_dt']
-    et_dt = json['et_dt']
-    method = RoomDAO()
-    data = method.find_available_rooms(st_dt, et_dt)
-    if not data:
-        return jsonify("Room Not Found"), 404
-    else:
-        result: list = []
-        for row in data:
-            result.append({
-                'r_id': row[0],
-                'room_name': row[1],
-                'room_type': row[2],
-                'url': 'https://booking-system-pika.herokuapp.com/pika-booking/rooms/' + str(row[0])
-            })
-        return jsonify(result), 200
+    # Retrieves all available rooms
+    def get_available_rooms(self, json):
+        st_dt = json['st_dt']
+        et_dt = json['et_dt']
+        method = RoomDAO()
+        available_rooms = method.find_available_rooms(st_dt, et_dt)
+        if not available_rooms:
+            return jsonify("Room Not Found"), 404
+        else:
+            result_list = []
+            for row in available_rooms:
+                obj = self.build_room(row[1:])
+                result_list.append(obj)
+            return jsonify(result_list)
--- models/Room.py
+++ models/Room.py
@@ -2,21 +2,13 @@ import psycopg2
 from config.dbcondig import db_root_config
 
 
-class RoomDAO(object):
+class RoomDAO:
     # CONSTANTS N STUFF
     T_LAB = 1
     T_CLASSROOM = 2
     T_CONFERENCE = 3
     T_OFFICE = 4
     T_STY_SPACE = 5
-
-    rooms = {
-        T_LAB: 'laboratory',
-        T_CLASSROOM: 'classroom',
-        T_OFFICE: 'office',
-        T_STY_SPACE: 'study_space',
-        T_CONFERENCE: 'conference_hall'
-    }
 
     def __init__(self):
         connection_url = "dbname=%s user=%s password=%s port=%s host=%s" % (db_root_config['dbname'],
@@ -26,63 +18,31 @@ class RoomDAO(object):
                                                                             db_root_config['host'])
         self.conn = psycopg2.connect(connection_url)
 
-    def __del__(self):
-        self.conn.close()
-
-    def check_if_room_exists(self, r_id):
-        cursor = self.conn.cursor()
-        query = 'select exists(select 1 from room where r_id = %s);'
-        cursor.execute(query, (r_id,))
-        result = cursor.fetchone()[0]
-        cursor.close()
-        return result
-
-    def count_rooms(self):
-        cursor = self.conn.cursor()
-        query = 'select count(*) as "count" ' \
-                'from room;'
-        cursor.execute(query,)
-        result = cursor.fetchone()[0]
-        cursor.close()
-        return result
-
     # Returns a query of all rooms
-    def get_all_rooms(self, limit_thingy: int) -> list:
+    def get_all_rooms(self):
         cursor = self.conn.cursor()
-        query = 'select r_id, r_name, r_type ' \
-                'from room limit %s;'
-        cursor.execute(query, (limit_thingy,))
-        result: list = []
+        query = 'select r_id, r_building, r_type, r_dept ' \
+                'from room;'
+        cursor.execute(query)
+        result = []
         for row in cursor:
             result.append(row)
-        cursor.close()
         return result
 
-    def get_room_by_id(self, r_id: int):
+    # GET Target Room using its id
+    def get_room(self, r_id: int):
         # Open Cursor for operations
         cursor = self.conn.cursor()
-        query = "select r_name, r_building, r_dept, r_type " \
-                "from room where r_id = %s;"
+        query = "select r_building, r_dept, r_type from room where r_id = %s;"
         # Execute commands n close
         cursor.execute(query, (r_id,))
         result = cursor.fetchone()
-        cursor.close()
-        return result
-
-    def get_room_by_name(self, r_name: str):
-        cursor = self.conn.cursor()
-        query = 'select r_id, r_building, r_dept, r_type ' \
-                'from room where r_name = %s;'
-        cursor.execute(query, (r_name,))
-        result = cursor.fetchone()
-        cursor.close()
         return result
 
     # Returns a query of all available rooms
     def find_available_rooms(self, st_dt, et_dt):
         cursor = self.conn.cursor()
-        query = 'select r_id, r_name, r_type from room ' \
-                'where room.r_id not in (select distinct r_id from room inner join (select ' \
+        query = 'select * from room where room.r_id not in (select distinct r_id from room inner join (select ' \
                 't.room_id,t.is_there_conflict from ( select room_id, st_dt, et_dt, tsrange(st_dt, et_dt) && tsrange(' \
                 '%s, %s) as is_there_conflict from (select availableroom.room_id, availableroom.st_dt, ' \
                 'availableroom.et_dt from availableroom UNION select booking.room_id, booking.st_dt, booking.et_dt ' \
@@ -91,50 +51,43 @@ class RoomDAO(object):
         result = []
         for row in cursor:
             result.append(row)
-        cursor.close()
         return result
 
     # creates a new room entry
-    def create_new_room(self, r_name: str, r_building: str, r_dept: str, r_type: int) -> int:
+    def create_new_room(self, r_building, r_dept, r_type):
         cursor = self.conn.cursor()
-        query = "insert into room (r_name, r_building, r_dept, r_type)  " \
-                "values (%s, %s, %s, %s) returning r_id;"
-        cursor.execute(query, (r_name, r_building, r_dept, r_type,))
+        query = "insert into room (r_building, r_dept, r_type)  values (%s, %s, %s) returning r_id; "
+        cursor.execute(query, (r_building, r_dept, r_type,))
         r_id = cursor.fetchone()[0]
         self.conn.commit()
-        cursor.close()
         return r_id
 
     # Updates an existing entry
-    def update_room(self, r_id: int, r_new_name: str, new_r_building: str, new_r_dept: str, new_r_type: int) -> bool:
+    def update_room(self, r_id, new_r_building, new_r_dept, new_r_type):
         cursor = self.conn.cursor()
-        query = "update room set r_name = %s, r_building = %s, r_dept = %s, r_type = %s " \
-                "where r_id = %s;"
-        cursor.execute(query, (r_new_name, new_r_building, new_r_dept, new_r_type, r_id,))
+        query = "update room set r_building = %s, r_dept = %s, r_type = %s where r_id = %s;"
+        cursor.execute(query, (new_r_building, new_r_dept, new_r_type, r_id,))
         self.conn.commit()
-        cursor.close()
         return True
 
     # Deletes an entry
-    def delete_room(self, r_id: int):
+    def delete_room(self, r_id):
         cursor = self.conn.cursor()
         query = 'delete from room ' \
                 'where r_id = %s;'
         cursor.execute(query, (r_id,))
         deleted_rows = cursor.rowcount
         self.conn.commit()
-        cursor.close()
         return deleted_rows != 0
 
     # Returns a query which is the most booked room
     def get_most_booked_rooms(self):
         cursor = self.conn.cursor()
-        query = 'select r_id, r_name, count(booking.room_id) as timed_booked ' \
+        query = 'select r_id, count(booking.room_id) as timed_booked ' \
                 'from booking inner join room on booking.room_id = room.r_id ' \
                 'GROUP BY r_id order by timed_booked desc limit 10; '
         cursor.execute(query)
         result = []
         for row in cursor:
             result.append(row)
-        cursor.close()
         return result
