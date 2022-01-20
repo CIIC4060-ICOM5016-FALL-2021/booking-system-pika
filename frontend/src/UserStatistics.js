d0c933ac03150e976191fcf291841ef85df68b4f
shulk72 <franklin.sifre@upr.edu>
2022-01-19 22:33:59 -0400

    more updates

--- frontend/src/UserStatistics.js
+++ frontend/src/UserStatistics.js
@@ -7,11 +7,12 @@ function UserStatistics (){
     let [data,setdata] =  useState("");
     let [d,setd] =  useState("");
     let[name,setname] =  useState("");
+    const [k,setk] =  useState(false);
     let e = localStorage.getItem("login-data");
     let   dat = JSON.parse(e)
     console.log(dat.p_id);
     function r() {
-
+      if (k=== false){
             axios.get(
                 `https://booking-system-pika.herokuapp.com//pika-booking/persons/${ dat.p_id}/most-booked-room`).then(res => {
                 setdata(res.data);
@@ -26,15 +27,29 @@ function UserStatistics (){
                 setname(res.data)
             })
         }
-
-
+}
+    function type1(parameter) {
+        switch (parameter) {
+            case 1:
+                return 'laboratory'
+            case 2:
+                return 'classroom'
+            case 3:
+                return 'office'
+            case 4:
+                return 'study_space'
+            case 5:
+                return 'conference_hall'
+        }
+    }
     useEffect(()=>{
         r()
+        setk(true)
     })
         return <>
 
                 <h1>  Most Used Room By You:
-             <h1> RoomName: {data.r_name} ,Building: {data.r_building} ,  Dept: {data.r_dept}  , Type: {data.r_type}  </h1>
+             <h1> RoomName: {data.r_name} ,Building: {data.r_building} ,  Dept: {data.r_dept}  , Type: {type1(data.r_type)}  </h1>
                 </h1>
             <h1>User most booked with You:    {name.p_fname} {name.p_lname}</h1>
                         <Link to = "/Dashboard" > <button>
